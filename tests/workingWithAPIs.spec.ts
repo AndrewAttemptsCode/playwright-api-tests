8615a869516577a2c6da78a01dec71ef6ec9b332
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2026-04-06 17:05:51 +0100

    test: add modification of intercepted api articles call title and description and asserting the updated data

--- tests/workingWithAPIs.spec.ts
+++ tests/workingWithAPIs.spec.ts
@@ -2,9 +2,22 @@ import { test, expect } from "@playwright/test";
 import tagData from "../test-data/tags.json";
 
 test.beforeEach(async ({ page }) => {
+  // Intercept and mock tags api
   await page.route("**/api/tags", async (route) => {
     await route.fulfill({
       json: tagData,
+    });
+  });
+
+  // Intercept articles api, modify latest article entry
+  await page.route("**/api/articles**", async (route) => {
+    const response = await route.fetch();
+    const data = await response.json();
+    data.articles[0].title = "New Title";
+    data.articles[0].description = "New description";
+
+    await route.fulfill({
+      json: data
     });
   });
 
@@ -16,4 +29,10 @@ test.beforeEach(async ({ page }) => {
 
 test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/conduit/i);
+ 
+});
+
+test("updates latest article title and description", async ({ page }) => {
+  await expect(page.locator("app-article-list").getByRole("heading", { name: /new title/i })).toBeVisible();
+  await expect(page.locator("app-article-list")).toContainText(/new description/i);
 });
